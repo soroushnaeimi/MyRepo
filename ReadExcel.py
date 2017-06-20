796e3db07cf7cd1f5d8bfade24eee4782b290991
soroushnaeimi <soroosh.gui@gmail.com>
2017-06-20 23:25:20 +1000

    read_excel add

---
+++ ReadExcel.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+xls = pd.read_excel('/home/soroush/Desktop/Managed_Wi-Fi_Cloud_Customer_Solution_Requirements_Tool.0.1.xlsm', sheetname='Organisation and Network Setup')
+xls.iloc[16,3]
